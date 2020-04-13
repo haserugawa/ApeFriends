6ae5e72d3f25233d617c5ddb56ec79f53cab6d2f
haserugawa <haseruneko@gmail.com>
2020-04-14 08:35:55 +0900

    認証機能実装

--- ApeFriends/routes/web.php
+++ ApeFriends/routes/web.php
@@ -11,8 +11,18 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
 
+*/
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
+
+Auth::routes();
+
+Route::get('/', function(){
+	return view('welcome');
+})->name('welcome');
+
+Route::get('/home', 'HomeController@index')->name('home');
